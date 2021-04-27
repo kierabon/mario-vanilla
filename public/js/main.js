3df1059d9124241e4685dc875cc43f92e4037ad4
kierab <78439567+kierabon@users.noreply.github.com>
2021-04-27 12:37:13 -0500

    Project background
    started npm, created ground and sky tiles

---
+++ public/js/main.js
@@ -0,0 +1,23 @@
+import SpriteSheet from './SpriteSheet.js';
+import {loadImage} from './loader.js';
+
+
+
+const canvas= document.getElementById("screen");
+const context= canvas.getContext("2d");
+
+context.fillRect(0,0,50,50);
+
+loadImage('/image/tiles.png')
+.then(image => {
+    const sprites = new SpriteSheet(image, 16, 16);
+    sprites.define('ground',0,0);
+    sprites.define('sky',3,23);
+    for (let x = 0; x< 25; ++x){
+        for (let y = 0; y < 14; ++y){
+            sprites.draw('sky', context, x*16, y*16);
+        }
+    }
+   
+    
+})
