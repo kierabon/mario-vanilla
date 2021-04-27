8c8f6a1f84ffa28ffcd9e2e8adc60d2183127899
kierab <78439567+kierabon@users.noreply.github.com>
2021-04-27 13:45:30 -0500

    finished background

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,15 @@
 import SpriteSheet from './SpriteSheet.js';
-import {loadImage} from './loader.js';
+import {loadImage, loadLevel} from './loader.js';
 
-
+function drawBackground( background, context, sprites){
+   background.ranges.forEach(([x1, x2, y1, y2]) => {
+      for (let x = x1; x < x2; ++x){
+        for (let y = y1; y < y2; ++y){
+            sprites.drawTile(background.tile, context, x, y);
+        }
+    } 
+   }); 
+}
 
 const canvas= document.getElementById("screen");
 const context= canvas.getContext("2d");
@@ -13,11 +21,15 @@ loadImage('/image/tiles.png')
     const sprites = new SpriteSheet(image, 16, 16);
     sprites.define('ground',0,0);
     sprites.define('sky',3,23);
-    for (let x = 0; x< 25; ++x){
-        for (let y = 0; y < 14; ++y){
-            sprites.draw('sky', context, x*16, y*16);
-        }
-    }
+   
+    loadLevel('1-1').then(level=> {
+        level.backgrounds.forEach(background => {
+        drawBackground(background, context, sprites);
+         });
+        });
+    
+     
+    
    
     
-})
+});
